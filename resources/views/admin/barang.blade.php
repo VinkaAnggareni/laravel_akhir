dd58ab6b4cb1c444a00c65cae38c39482df8ff7a
VinkaAnggareni <62175319+VinkaAnggareni@users.noreply.github.com>
2020-04-13 09:52:27 +0800

    laravel akhir

---
+++ resources/views/admin/barang.blade.php
@@ -0,0 +1,54 @@
+@extends(layouts.tes)
+@section('content')
+<body>
+
+  <div class="overlay"></div>
+  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="mp4/bg.mp4" type="video/mp4">
+  </video>
+
+  <div class="masthead">
+    <div class="masthead-bg"></div>
+    <div class="container h-100">
+          </div>
+
+
+  <div class="social-icons">
+    <ul class="list-unstyled text-center mb-0">
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+      </li>
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="http://localhost/laravel_vinka/public/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="http://localhost/laravel_vinka/public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/coming-soon.min.js"></script>
+
+
+
+
+</div>
+<div class"content">
+    <div class="panel panel-flat border-top-lg border-top-primary">
+        <div class="panel-body">
+            <table class="table table-bordered">
+
+            </table>
+</body>
+@endsection
---
+++ resources/views/admin/tescontent.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.tes')
+@section('content')
+<div class="panel">
+     <div class="panel-body">
+         <h1> Jenis Barang</h1>
+        <table class="table bordered">
+            <tr>
+                <td width="400"><br> BIODATA</br></td>
+                <td><br> STUDI KASUS</br></td>
+
+            </tr>
+            <tr>
+                <td><br> NAMA  : NENGAH AYU VINKA ANGGARENI</br></td>
+                <td><br> JUDUL : Sistem Manajemen Toko Kelontong(SJubel)</br></td>
+
+            </tr>
+            <tr>
+                <td><br> NIM  : 1815051073</br></td>
+                <td><br> PENJELASAN :SJubel merupakan kepanjangan dari sistem jual beli, dimana sistem tersebut akan digunakan dalam transaksi jual beli pada toko kelontong </br></td>
+
+            </tr>
+            <tr>
+                <td><br> PROGRAM STUDI : PENDIDIKAN TEKNIK INFORMATIKA</br></td>
+               
+
+            </tr>
+        </table>
+
+    </div>
+ </div>
+@endsection
